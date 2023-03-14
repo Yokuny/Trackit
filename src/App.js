0228da3ec5093276cecf4361e381fb22d3dba6d5
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-03-14 20:36:16 -0300

    The header and footer are prepared

--- src/App.js
+++ src/App.js
@@ -2,29 +2,36 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/login/LoginForm";
 import RegisterForm from "./components/login/RegisterForm";
 import Login from "./components/login/Login";
-
+import Hoje from "./components/hoje/Hoje.js";
+import styled from "styled-components";
+const ScreenSize = styled.div`
+  width: 375px;
+`;
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Login>
-              <LoginForm />
-            </Login>
-          }
-        />
-        <Route
-          path="/cadastro"
-          element={
-            <Login>
-              <RegisterForm />
-            </Login>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <ScreenSize>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Login>
+                <LoginForm />
+              </Login>
+            }
+          />
+          <Route
+            path="/cadastro"
+            element={
+              <Login>
+                <RegisterForm />
+              </Login>
+            }
+          />
+          <Route path="/hoje" element={<Hoje />} />
+        </Routes>
+      </BrowserRouter>
+    </ScreenSize>
   );
 };
 export default App;
---
+++ src/components/hoje/Hoje.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import Header from "../Header.js";
+import Footer from "../Footer.js";
+const Ta = styled.div`
+  background-color: #e5e5e5;
+  width: 100%;
+  height: 100vh;
+`;
+const Hoje = () => {
+  return (
+    <>
+      <Header />
+      <Ta>a</Ta>
+      <Footer />
+    </>
+  );
+};
+export default Hoje;
