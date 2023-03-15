39c2a9c4897d46d2065811506f9b3a19627d9f55
yokuny <felipe.vni@hotmail.com>
2023-03-14 22:00:58 -0300

    today's page structure done
    now I have to consume the api

--- src/components/today/Today.js
+++ src/components/today/Today.js
@@ -1,19 +1,32 @@
 import styled from "styled-components";
 import Header from "../Header.js";
 import Footer from "../Footer.js";
+import TaskCard from "./TaskCard.js";
 const FullScreen = styled.div`
   background-color: #e5e5e5;
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: 100vh;
-  padding: calc(28px + 70px) 18px 70px 18px;
+  box-sizing: border-box;
+  padding-top: calc(28px + 70px);
+  padding-bottom: calc(70px + 10px);
 `;
-const TodayInfo = styled.div``;
-const TaskCard = styled.div`
+const TodayInfo = styled.div`
+  margin-bottom: 28px;
   width: 340px;
-  height: 94px;
-  background: #ffffff;
-  border-radius: 5px;
+  p:first-child {
+    font-size: 23px;
+    line-height: 29px;
+    color: #126ba5;
+  }
+  p:last-child {
+    font-size: 18px;
+    line-height: 22px;
+    color: #bababa;
+  }
 `;
+
 const Today = () => {
   return (
     <FullScreen>
@@ -22,14 +35,7 @@ const Today = () => {
         <p>Segunda, 17/05</p>
         <p>Nenhum hábito concluído ainda</p>
       </TodayInfo>
-      <TaskCard>
-        <div>
-          <p>Ler 1 capítulo de livro</p>
-          <p>Sequência atual: 3 dias Seu recorde: 5 dias</p>
-          <p>Seu recorde: 5 dias</p>
-        </div>
-        <img src="" alt="check icon" />
-      </TaskCard>
+      <TaskCard />
       <Footer />
     </FullScreen>
   );
